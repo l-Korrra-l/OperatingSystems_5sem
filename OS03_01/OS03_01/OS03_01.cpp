5e520c4f06fe26e75e0876e4b619e86ccd9164c0
l-Korrra-l <64279692+l-Korrra-l@users.noreply.github.com>
2021-09-24 12:36:49 +0300

    lab 03

---
+++ OS03_01/OS03_01/OS03_01.cpp
@@ -0,0 +1,15 @@
+﻿// OS03_01.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <process.h>
+
+int main()
+{
+    for (int i = 0; i < 10000; i++) {
+        Sleep(1000);
+        std::cout << "\t" << _getpid();
+    }
+   
+}
---
+++ OS03_01/OS03_02/OS03_02.cpp
@@ -0,0 +1,20 @@
+﻿// OS03_02.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    LPCWSTR an = L"C:\\Users\\User\\Documents\\ОСиСП\\OperationSystems_5sem\\OS03_01\\Debug\\OS03_02.exe";
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+
+    if ( //child process
+        CreateProcess(an, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)
+        )
+    std::cout << "---Proc created"
+    std::cout << "Hello World!\n";
+}
