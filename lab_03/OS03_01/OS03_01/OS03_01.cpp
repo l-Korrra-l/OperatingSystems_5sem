fafcf1074e6a309de8c9ce4aa6e4f792a187aad6
l-Korrra-l <64279692+l-Korrra-l@users.noreply.github.com>
2021-11-10 09:07:15 +0300

    lab 03 c++

---
+++ lab_03/OS03_01/OS03_01/OS03_01.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <process.h>
+
+int main()
+{
+    for (int i = 0; i < 10000; i++) {
+        Sleep(1000);
+        std::cout << "\t" << _getpid();
+    }
+   
+}
